5343af1b0e0c761bfa2782c2da0216e5cc223ffa
Ricardo Tomasi <ricardobeat@gmail.com>
2011-05-23 19:28:25 -0300

    add page scrolling plugin

---
+++ source/jquery.scrollTo.js
@@ -0,0 +1,40 @@
+/* 
+ * jQuery.scrollTo
+ *
+ * Copyright 2010, Grifo Tecnologia
+ * Licensed under the MIT License 
+ *
+ * animated page scrolling
+ * @author Ricardo Tomasi <ricardo.tomasi@grifotecnologia.com.br>
+ *
+ */
+;(function($){
+
+function _scroll(){
+	
+	var doc = document.documentElement || document.body
+	  , top = $(this).offset().top
+
+	if (top > doc.scrollTop)
+		return this
+
+	$(doc).animate({
+		scrollTop: top-(margin||30)
+	}, Math.min(doc.scrollTop-top+200, 1500))
+	
+}
+
+$.fn.scrollTo = function(queue, margin){
+
+	if (!!queue){
+		this.eq(0).queue('fx', function(){
+			_scroll.call(this)
+			$(this).dequeue()
+		})
+	} else {
+		_scroll.call(this[0])
+	}
+	
+	return this
+}
+})(jQuery);
