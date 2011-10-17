91a34ac48c0d096f6537634e0c5dc9183280178c
Ricardo Tomasi <ricardobeat@gmail.com>
2011-10-17 21:30:22 -0200

    modern javascript

--- source/jquery.autotab.js
+++ source/jquery.autotab.js
@@ -8,45 +8,45 @@
  * @author Jean Carlo Emer <jean@gri.fo>
  *
  */
- ;(function($){
-	$.fn.autotab = function(opts){
-		var options = $.extend({
-			parent: 'form',
-			elements: ['input', 'select', 'textarea', 'button', 'a'],
-			itens: function(target){
-				return $(target).closest(options.parent)
-					.find(options.elements.join(','));
-			},
+;(function($){
+    $.fn.autotab = function(opts){
+        var options = $.extend({
+            parent: 'form'
+          , elements: 'input, select, textarea, button, a'
+          , items: function(target){
+                return $(target).closest(options.parent).find(options.elements)
+            }
+          , refresh: false
+          , mask_set: '_'
+          , mask_to: ''
+          , maxlength_track: 100
+        }, opts)
 
-			refresh: false,
-			mask_set: '_',
-			mask_to: '',
-			maxlength_track: 100
-		}, opts);
+        return this.each(function(){
+            var self = $(this)
+              , maxlength = self.attr('maxlength')
+              , items
+            
+            if (maxlength && maxlength <= options.maxlength_track){
+                self.focus(function(){
+                    self.data('printable', false)
+                    if (options.refresh || !items){
+                        items = options.items(self)
+                    }
+                })
 
-		return this.each(function(){
-			var self = $(this),
-				maxlength = self.attr('maxlength'),
-				itens;
-			
-			if(maxlength && maxlength <= options.maxlength_track){
-				self.focus(function(){
-					self.data('printable', false);
-					(options.refresh || !itens) && (itens = options.itens(self));
-				});
+                self.keypress(function(e){
+                    self.data('printable', e.which !== 0 && e.charCode !== 0)
+                })
 
-				self.keypress(function(e){
-					self.data('printable', e.which !== 0 && e.charCode !== 0);
-				});
-
-				self.keyup(function(e){
-					if(
-						self.data('printable') &&
-						self.val().replace(options.mask_set, options.mask_to).length >= maxlength
-					)
-						itens.eq(itens.index(self) + 1).focus();
-				});
-			}
-		});
-	}
+                self.keyup(function(e){
+                    if (self.data('printable')
+                        && self.val().replace(options.mask_set, options.mask_to).length >= maxlength
+                    ){
+                        items.eq(items.index(self) + 1).focus()
+                    }
+                })
+            }
+        })
+    }
 })(jQuery);
